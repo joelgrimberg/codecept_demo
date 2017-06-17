d391d31a5e526ed26487569688ca6befcba7275d
Joël Grimberg <github@joelgrimberg.nl>
2017-06-17 11:26:59 +0200

    tweaking travis config

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,6 +5,8 @@ exports.config = {
   "helpers": {
     "WebDriverIO": {
       // load variables from the environment and provide defaults
+      "user": "joel-grimberg",
+      "key": "05bb5bca-6c41-4ca7-b1d8-0efecb1439c7",
       "url": "https://www.bol.com",
       "browser": "chrome",
       //  "browser" : process.profile || "phantomjs",
